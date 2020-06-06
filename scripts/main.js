9b698321d503491666b0beb4514c793fade2ecb3
Chandra Prakash <pr.chandraprakash@gmail.com>
2020-06-06 21:44:46 +0530

    fix one off errors in timer logic

--- scripts/main.js
+++ scripts/main.js
@@ -2,16 +2,19 @@ function startTimer() {
     let time = display.textContent.split(':');
     let minute = time[0];
     let second = time[1];
+    console.log(minute, second);
+    if(minute === '0') console.log(typeof(minute));
+    const timerId = setInterval( () => {
+        if(second == 1 && minute == 0) clearInterval(timerId);
 
-    setInterval( () => {
         if(second === "00") {
             minute --;
             second = "60";
         }
         second --;
-        if(minute == '0') minute = '0' + minute;
-        if(second < 10) minute = '0' + minute;
-
+        if(minute === '0') minute = '0' + minute;
+        if(second < 10) second = '0' + second;
+        console.log(minute, second);
         display.textContent = minute + ":" + second;
     }, 1000);
 }
